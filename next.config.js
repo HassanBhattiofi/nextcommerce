29bfbc83354e92ffe4791ee5dd28272ed4343d78
Hassan Bhatti <hasha361@gmail.com>
2021-06-15 00:23:32 +0500

    first commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    BASE_URL: "http://localhost:3000",
+    MONGODB_URL:
+      "mongodb+srv://hassan:hassan@nextcommerce.ffgs3.mongodb.net/nextcommerce?retryWrites=true&w=majority",
+    ACCESS_TOKEN_SECRET: "1234567",
+    REFRESH_TOKEN_SECRET: "1234567",
+    PAYPAL_CLIENT_ID: "YOUR_PAYPAL_CLIENT_ID",
+    CLOUD_UPDATE_PRESET: "ml_default",
+    CLOUD_NAME: "bokking",
+    CLOUD_API: "tBSU9sIFbtt__YI4zCTxxWDWibU",
+  },
+};
